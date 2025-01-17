b6d70358d027d999387fca3f8477a093ceacf183
dy7791 <dy7791@naver.com>
2025-01-17 15:49:18 +0900

    fix:: code split

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,28 @@
 // Common
 import ErrorBoundary from "./common/ErrorBoundary";
 import Header from "./common/Header";
+import Loading from "./common/Loading";
 import RenderComponent from "./common/RenderComponent";
 
 // Introduce
 import Welcome from "./introduce/Welcome";
 import AboutMe from "./introduce/AboutMe";
 import AboutMeItem from "./introduce/AboutMeItem";
+import Skill from "./introduce/Skill";
+import SkillItem from "./introduce/SkillItem";
+import Project from "./introduce/Project";
+import ProjectItem from "./introduce/ProjectItem";
 
 export {
   ErrorBoundary,
   Header,
+  Loading,
   RenderComponent,
   Welcome,
   AboutMe,
   AboutMeItem,
+  Skill,
+  SkillItem,
+  Project,
+  ProjectItem,
 };
