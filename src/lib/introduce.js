8484e64bb378ba44ac537546161790bc9e02e283
dy7791 <dy7791@naver.com>
2025-01-17 10:18:46 +0900

    add:: introduce_data > temp

---
+++ src/lib/introduce.js
@@ -0,0 +1,74 @@
+export const introduce_data = [
+  {
+    id: 0,
+    title: "1번 제목",
+    content: [
+      {
+        subtitle: "첫번째 소제목",
+        subContnet: "첫번째 소제목의 내용",
+      },
+      {
+        subtitle: "두번째 소제목",
+        subContnet: "두번째 소제목의 내용",
+      },
+      {
+        subtitle: "세번째 소제목",
+        subContnet: "세번째 소제목의 내용",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "2번 제목",
+    content: [
+      {
+        subtitle: "첫번째 소제목",
+        subContnet: "첫번째 소제목의 내용",
+      },
+      {
+        subtitle: "두번째 소제목",
+        subContnet: "두번째 소제목의 내용",
+      },
+      {
+        subtitle: "세번째 소제목",
+        subContnet: "세번째 소제목의 내용",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "3번 제목",
+    content: [
+      {
+        subtitle: "첫번째 소제목",
+        subContnet: "첫번째 소제목의 내용",
+      },
+      {
+        subtitle: "두번째 소제목",
+        subContnet: "두번째 소제목의 내용",
+      },
+      {
+        subtitle: "세번째 소제목",
+        subContnet: "세번째 소제목의 내용",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "4번 제목",
+    content: [
+      {
+        subtitle: "첫번째 소제목",
+        subContnet: "첫번째 소제목의 내용",
+      },
+      {
+        subtitle: "두번째 소제목",
+        subContnet: "두번째 소제목의 내용",
+      },
+      {
+        subtitle: "세번째 소제목",
+        subContnet: "세번째 소제목의 내용",
+      },
+    ],
+  },
+];
