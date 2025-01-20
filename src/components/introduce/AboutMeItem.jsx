78d1fcdb57eecc1b332badb61f72e641bd251dd7
DongYeop Lee <dy7791@naver.com>
2025-01-20 22:59:50 +0900

    add:: Modal

--- src/components/introduce/AboutMeItem.jsx
+++ src/components/introduce/AboutMeItem.jsx
@@ -1,9 +1,31 @@
+import { Box, Modal } from "@mui/material";
 import styled from "styled-components";
+import { useModal } from "../../hooks";
 
 function AboutMeItem({ items }) {
+  const { open, handleOpen, handleClose } = useModal();
+
   return (
     <ItemWrapper className="item-wrapper">
-      <div className="item-box">{items.title}</div>
+      <div className="item-box" onClick={handleOpen}>
+        {items.title}
+      </div>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={ModalStyle}>
+          {items.content.map((item, idx) => {
+            return (
+              <Box key={idx}>
+                <Box>
+                  <strong>{item.subtitle}</strong>
+                </Box>
+                <br />
+                <Box>{item.subcontent}</Box>
+                <br />
+              </Box>
+            );
+          })}
+        </Box>
+      </Modal>
     </ItemWrapper>
   );
 }
@@ -28,3 +50,15 @@ const ItemWrapper = styled.div`
     box-shadow: 8px 7px 1px 1px #efefef;
   }
 `;
+
+const ModalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
